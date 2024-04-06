25a1a40681e72e930b934936f38329d4176e337e
Nareshram1 <nireshram805s1@gmail.com>
2024-04-07 01:15:28 +0530

    Login/Signup

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
+}
+
+var client *mongo.Client
+
+func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
+    response.Header().Set("content-type", "application/json")
+    var user User
+    _ = json.NewDecoder(request.Body).Decode(&user)
+
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+    if err != nil {
+        response.WriteHeader(http.StatusInternalServerError)
+        response.Write([]byte(`{"error": "Error hashing password"}`))
+        return
+    }
+
+    collection := client.Database("npdb").Collection("users")
+    _, err = collection.InsertOne(context.Background(), User{Username: user.Username, Password: string(hashedPassword)})
+    if err != nil {
+        response.WriteHeader(http.StatusInternalServerError)
+        response.Write([]byte(`{"error": "` + err.Error() + `"}`))
+        return
+    }
+
+    json.NewEncoder(response).Encode(user)
+}
+
+func LoginUserEndpoint(response http.ResponseWriter, request *http.Request) {
+    response.Header().Set("Content-Type", "application/json")
+    var user User
+    _ = json.NewDecoder(request.Body).Decode(&user)
+
+    collection := client.Database("npdb").Collection("users")
+    filter := bson.M{"username": user.Username}
+    var result User
+    err := collection.FindOne(context.Background(), filter).Decode(&result)
+    if err != nil {
+        response.WriteHeader(http.StatusUnauthorized)
+        response.Write([]byte(`{"error": "Invalid username or password"}`))
+        return
+    }
+
+    // Check if the password matches
+    if err := bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(user.Password)); err != nil {
+        response.WriteHeader(http.StatusUnauthorized)
+        response.Write([]byte(`{"error": "Invalid username or password"}`))
+        return
+    }
+    // If login is successful, return a success message
+    response.WriteHeader(http.StatusOK)
+    response.Write([]byte(`{"message": "Login successful"}`))
+}
+
+
+
+func main() {
+    // Load environment variables from .env file
+    if err := godotenv.Load(); err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+
+    // Get MongoDB URI from environment variable or use a default value
+    mongoURI := os.Getenv("MONGO_URI")
+	log.Println(mongoURI)
+    if mongoURI == "" {
+        log.Fatal("MONGO_URI environment variable is not set")
+    }
+    // Set up MongoDB client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+    clientOptions := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
+    client, _= mongo.Connect(context.Background(), clientOptions)
+	log.Println("Connected to database server.")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000" // Default port
+	}
+
+    router := mux.NewRouter()
+    router.HandleFunc("/signup", CreateUserEndpoint).Methods("POST")
+	router.HandleFunc("/login", LoginUserEndpoint).Methods("POST")
+	// Use CORS middleware to handle CORS
+	handler := cors.Default().Handler(router)
+    log.Fatal(http.ListenAndServe(":"+port, handler))
+}
